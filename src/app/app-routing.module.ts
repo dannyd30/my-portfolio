ebb1efa746f76fe0bfc446efd01656210347c4f2
dannyd30 <32637204+dannyd30@users.noreply.github.com>
2020-06-22 21:22:25 +0530

    Added more details and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: 'home', component: HomeComponent},
+  { path: 'vmore', component: AboutComponent},
+  { path: 'intouch', component: ContactComponent},
+  // { path: 'works', component: Pro}
+  {
+    path: "**", component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { WorkComponent } from './work/work.component';
+import { BannerComponent } from './banner/banner.component';
+import { InterestsComponent } from './interests/interests.component';
+import { CareerComponent } from './career/career.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    WorkComponent,
+    BannerComponent,
+    InterestsComponent,
+    CareerComponent
   ],
   imports: [
     BrowserModule,
